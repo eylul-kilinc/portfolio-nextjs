0d103b9a15c8b80c7885c6d41442a8b50bb65564
eylul-kilinc <eylulkilinc34@gmail.com>
2025-12-31 22:47:11 +0300

    j

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import ConditionalNavbar from "@/components/conditional-navbar";
-import Script from "next/script";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -62,7 +61,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="overflow-x-hidden">
+      <body className="overflow-x-hidden" suppressHydrationWarning>
         <ConditionalNavbar />
         {children}
         {/* JSON-LD Person Schema for Google */}
